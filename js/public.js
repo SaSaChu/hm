45de5f850fe98b9c3bab0d4159a2ff80fd23b292
OA Wu <comdan66@gmail.com>
2016-09-04 16:27:21 +0800

    Tmp

--- js/public.js
+++ js/public.js
@@ -5,5 +5,23 @@
 
 $(function () {
   // 圖自動縮放
-   $('.banners').imgLiquid ({verticalAlign: 'center'});
+  $('._i').imgLiquid ({verticalAlign: 'center'});
+
+  $('.banners').each (function () {
+    var $that = $(this);
+    var $pagination = $that.find ('.pagination > div > a');
+
+    $pagination.click (function () {
+      $(this).addClass ('active').siblings ().removeClass ('active');
+      $that.attr ('class', 'banners n' + ($(this).index () + 1));
+    }).first ().click ();
+
+    setInterval (function () {
+      var index = $pagination.filter ('.active').index () + 1;
+      index = index >= $pagination.length ? 0 : index;
+      $pagination.eq (index).click ();
+    }, 7 * 1000);
+  });
+
+  
 });
